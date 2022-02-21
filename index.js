089ede6e9551a1e1f60da4f9850b2b3d5110e33a
Kevin <ghekghek2@gmail.com>
2022-02-21 15:24:00 +0800

    design a work slider

--- index.js
+++ index.js
@@ -151,23 +151,34 @@ const Cards = ((() => {
       cards[n].classList.remove("card--left");
       cards[n].classList.remove("card--center");
       cards[n].classList.remove("card--right");
+   
+     
     }
     cards[card].classList.add("card--center");
     if(card == 0)
     {
       cards[2].classList.add("card--left");
+
       cards[1].classList.add("card--right");
+     
+      
     }
     if(card == 1)
     {
       cards[0].classList.add("card--left");
       cards[2].classList.add("card--right");
+
     }
     if(card == 2)
     {
+    
       cards[1].classList.add("card--left");
+  
       cards[0].classList.add("card--right");
+    
     }
+
+
   }
 
   return {
